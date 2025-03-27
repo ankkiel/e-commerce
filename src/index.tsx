2a359ab5e29a0e8e182b9cb656389679bcec6d7b
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2025-03-27 15:23:30 +0300

    feat: изменяет структуру проекта

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom/client';
+import App from './App';
+
+import './styles/index.css';
+
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
