b16a6464e3d8053ec23b689eb6c823d95c6f2432
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2025-03-29 15:23:07 +0300

    feat: реализует компонент Card

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -5,7 +5,7 @@ export type TextProps = {
   /** Дополнительный класс */
   className?: string;
   /** Стиль отображения */
-  view?: 'title' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
+  view?: 'title' | 'subtitle' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
   /** Html-тег */
   tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'p' | 'span';
   /** Начертание шрифта */
