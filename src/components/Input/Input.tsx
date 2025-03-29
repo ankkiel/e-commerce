b255ba9d31a0b52b0837edb736b47aaafc21518b
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2025-03-29 21:09:03 +0300

    fix: исправляет компонент Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import react, { useState } from 'react';
+import { forwardRef } from 'react';
 import Icon from 'components/icons/ArrowDownIcon';
 import styleText from '../Text/Text.module.scss';
 import style from './Input.module.scss';
@@ -13,14 +13,12 @@ export type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onCh
   afterSlot?: React.ReactNode;
 };
 
-const Input = react.forwardRef<HTMLInputElement, InputProps>(
-  ({ value: initialValue, onChange: onChangeProp, className, placeholder, disabled, afterSlot, ...props }, ref) => {
-    const [value, setValue] = useState(initialValue || '');
-
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const newValue = e.target.value;
-      setValue(newValue);
-      onChangeProp?.(newValue);
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ value, onChange, disabled, placeholder, afterSlot, className, ...props }, ref) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (onChange) {
+        onChange(event.target.value);
+      }
     };
 
     return (
@@ -28,11 +26,12 @@ const Input = react.forwardRef<HTMLInputElement, InputProps>(
         <input
           {...props}
           type="text"
+          onChange={handleChange}
           value={value}
-          onChange={onChange}
           placeholder={placeholder}
           disabled={disabled}
           className={cn(style.input, styleText['text_p-16'])}
+          ref={ref}
         />
         {afterSlot && (
           <div className={style['input__icon']}>
